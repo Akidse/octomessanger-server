043acd519c1c7c99c0cb63ca9f1d2a3b5ce48ed5
AkidSe <akidse1@gmail.com>
2020-12-11 22:13:00 +0200

    heroku app settings

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -2,7 +2,7 @@ import { IEnvironment } from "./env"
 
 const environment: IEnvironment = {
     MONGO_DB_LINK: process.env.MONGO_DB_LINK || '',
-    SERVER_PORT: process.env.SERVER_PORT || '',
+    SERVER_PORT: process.env.SERVER_PORT || process.env.PORT || '' ,
     SOCKET_IO_CORS_ORIGIN: process.env.SOCKET_IO_CORS_ORIGIN || '',
     ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || '',
 };
